45568c877fae06d700c44b84b3b5d7702b2da8ed
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2025-05-23 15:44:42 +0100

    update basepath, remove unused gitlab-ci.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 import nextMdx from "@next/mdx";
 import remarkGfm from "remark-gfm";
 
-// img-src 'self' blob: data:;
-// default-src 'self';
-// added analytic scripts for no CSP blocking
-const cspHeader = `
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://static.hotjar.com;
-    style-src 'self' 'unsafe-inline';
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-`;
-
 const withMDX = nextMdx({
 	extension: /\.mdx?$/,
 	options: {
@@ -25,8 +10,8 @@ const withMDX = nextMdx({
 });
 
 const nextConfig = {
-	basePath: "/cancer-models",
-	assetPrefix: "/cancer-models",
+	basePath: "/cancer-models-pages",
+	assetPrefix: "/cancer-models-pages",
 	reactStrictMode: true,
 	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 	swcMinify: true,
